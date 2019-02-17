604bec03520fec895aad5225122d52ddafbe792e
Calberk <park.edmund@gmail.com>
2019-02-17 00:06:41 -0800

    add proptypes to all pages with props

--- catch-of-the-day/src/components/storePicker.js
+++ catch-of-the-day/src/components/storePicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component{
@@ -8,6 +9,10 @@ class StorePicker extends React.Component{
     // }
 
     myInput = React.createRef();  //creating a ref
+
+    static propTypes = {
+        hisotry: PropTypes.object
+    }
 
     goToStore = event => {
         // 1. Stop the form from submitting
